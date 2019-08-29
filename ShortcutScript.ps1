5f33569d0359a31e0959b0cf9ac8d3caa0afc2fe
dgbr <30595930+dgbr@users.noreply.github.com>
2019-08-29 13:27:38 +1000

    Create ShortcutScript.ps1

---
+++ ShortcutScript.ps1
@@ -0,0 +1,3 @@
+Write-Host "Entering CRM_Demi_Brauer.ps1..."
+<DIRECTORY>\CRM_Shortcuts3\SetupTools.ps1 -crmShortcutsDir <DIRECTORY>\CRM_Shortcuts3
+<DIRECTORY>\CRM_Shortcuts3\LoginToDynamics.ps1 -url <URL> -username <USERNAME> -password <PASSWORD> -crmShortcutsDir <DIRECTORY>\CRM_Shortcuts3
