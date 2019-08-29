59f5e2fd1f54aead32146ed0d6778f5ec11be2fe
dgbr <30595930+dgbr@users.noreply.github.com>
2019-08-29 14:15:18 +1000

    Prompt user to input username and password to encrypt and reuse

--- ShortcutScript.ps1
+++ ShortcutScript.ps1
@@ -1,3 +1,25 @@
-Write-Host "Entering CRM_Demi_Brauer.ps1..."
+param(
+  [Parameter(Mandatory=$true, Position=1)]
+	[string]$crmShortcutsDir,
+  [Parameter(Mandatory=$true, Position=2)]
+	[string]$instanceName
+)
+
+Write-Host "Entering ShortcutScript.ps1..."
+
+# Create Credentials folder
+$credentialsPath = "$crmShortcutsDir\Credentials"
+if (-not (Test-Path $credentialsPath)){
+    md $credentialsPath
+}
+
+# Store encrypted credentials if they're not already stored in the Credentials folder
+if(![System.IO.File]::Exists("$credentialsPath\$instanceName")){
+	Get-Credential | Export-CliXml -Path "$credentialsPath\$instanceName"
+}
+
+#Retrieve credentials from encrypted xml file
+$credentials = Import-Clixml -Path "$credentialsPath\$instanceName"
+
 <DIRECTORY>\CRM_Shortcuts3\SetupTools.ps1 -crmShortcutsDir <DIRECTORY>\CRM_Shortcuts3
 <DIRECTORY>\CRM_Shortcuts3\LoginToDynamics.ps1 -url <URL> -username <USERNAME> -password <PASSWORD> -crmShortcutsDir <DIRECTORY>\CRM_Shortcuts3
